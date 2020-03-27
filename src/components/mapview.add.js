91662e68d00bc5e9b38527382b667858ed966346
Jarek Pelczynski <jpelczynski@gmail.com>
2020-03-27 19:35:13 +0100

    Change about & contact page

--- src/components/mapview.add.js
+++ src/components/mapview.add.js
@@ -299,48 +299,6 @@ export function MapAddComponent() {
           </div>
         </div>
       )}
-
-      <div className="ui vertical segment">
-        <div className="ui text container formcontainer">
-          <h2>O stronie</h2>
-          Strona powstała z myśla zebrania w jednym miejscu lokalnego biznesu,
-          aby przez nadane przez rząd ograniczenia spowodu pandemi, mogłby one
-          przetwać a takze, aby ułatwić mieszkańcom znalezienie miejsca, z
-          którego będą mogli zamówić jedzenie. Strona powstała w oparciu o
-          projekt udostępniony przez{' '}
-          <a
-            href="https://medium.com/@marcfehr/how-to-build-a-fast-and-reliable-community-mapping-tool-with-gatsbyjs-and-firebase-74a0fa6b5b83"
-            target="_blank"
-          >
-            Marc Fehr
-          </a>{' '}
-          na{' '}
-          <a
-            href="https://gitlab.com/marc.fehr/community-isolation-map"
-            target="_blank"
-          >
-            GitLab
-          </a>{' '}
-          jako projekt Open Source, który podobnie jak ten, ma pomóc lokalnej
-          spłeczności w Muizenberg, w Południowej Afryce.
-          <h2>Jakie miejca można dodawać?</h2>
-          Wszystkie miejsca spełniajace lokalnego przedsiębiorstwa, czyli
-          restauracje, bary, kawiarnie, cukiernie, piekarnie, które oferuja w
-          czasie ograniczeń opcję na wynos lub dostawę (własna lub przez
-          platformy Glovo, Pyszne.pl, Uber Eats).
-          <h2>Co jeśli chcę usunąć dane z mapy</h2>
-          Jeśli nie chcesz, żeby miejsce nie było wyświetlane na mapie{' '}
-          <strong>napisz do mnie</strong>.<h2>Czy strona będzie ulepszana?</h2>
-          Strona będzie cały czas rozwijana i ulepszana. Kazdy feedback jest
-          mile widziany, jeśli masz pomysł jak usprawnić działanie strony to
-          napisz.
-          <h2>Dlaczego tylko Kraków?</h2>
-          Kraków miejscem, w którym mieszkam i sam potrzebuję
-          takiej mapy z miejscami, gdzie mogę zamówić coś do jedzenia. Aplikacja
-          jest udostępniona na zasadach Open Srouce i kazdy moze postawić swoja
-          wersja dla swojego miasta – jeśli masz tylko wiedzę i odrobinę ochoty.
-        </div>
-      </div>
     </div>
   );
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,40 +8,56 @@ const Contact = () => {
   return (
     <Layout>
       <Head title={`Contact`} />
-      <section className='ui vertical very fitted segment' style={{marginTop: '1rem'}}>
-        <div className='ui container'>
-          <h1 className='ui header'>
-            <div className='content'>
-            <span className='page-title'>
-              We must act now.
-            </span>
-              <div className='sub header'>
-                Get in touch with the MUIZ-CAN network and find out more. If you are looking for information about the COVID-19 pandemic, visit our <Link to={'/'}>Information</Link> page.
-              </div>
-              <Link to={'/'} className={'ui black icon button'}>
-                <i className={'icon map'}/> Go to the map
-              </Link>
-            </div>
-          </h1>
-        </div>
-      </section>
-
-      <section className='ui vertical segment' style={{minHeight: '55vh'}}>
-        <div className='ui text container formcontainer'><div className={'ui yellow message'} style={{fontWeight: 500, fontStyle: 'italic'}}>
-          <i className={'icon external link'} />Read the Tutorial about how to set up your own map <a href={'https://medium.com/@marcfehr/how-to-build-a-fast-and-reliable-community-mapping-tool-with-gatsbyjs-and-firebase-74a0fa6b5b83?source=userActivityShare-f57d26da4972-1584988662&_branch_match_id=689400773593121406'} target={'_blank'} rel={'noopener noreferrer'}>here</a>.
-        </div>
-          <h2>Reach out via email</h2>
-          <ul>
-            <li>
-              {/* If you're a developer, add yourself here and create a MR on Gitlab do get onto the original repository */}
-              <a href='mailto:marc.fehr@gmail.com'>
+      <section
+        className="ui vertical very fitted segment"
+        style={{ marginTop: '1rem' }}
+      >
+        <div className="ui container">
+          <div className="ui vertical segment">
+            <div className="ui text container">
+              <h2>Kontakt</h2>
+              Masz pytanie? <a href="mailto:krakowcommunityfoodmap@pm.me"><strong>Napisz!</strong></a>
+              <h2>O stronie</h2>
+              Strona powstała z myśla zebrania w jednym miejscu lokalnego
+              biznesu, aby przez nadane przez rząd ograniczenia spowodu pandemi,
+              mogłby one przetwać a takze, aby ułatwić mieszkańcom znalezienie
+              miejsca, z którego będą mogli zamówić jedzenie. Strona powstała w
+              oparciu o projekt udostępniony przez{' '}
+              <a
+                href="https://medium.com/@marcfehr/how-to-build-a-fast-and-reliable-community-mapping-tool-with-gatsbyjs-and-firebase-74a0fa6b5b83"
+                target="_blank"
+              >
                 Marc Fehr
-              </a>, developer and maintainer of{' '}
-              <a href={'https://www.whozinberg.org'} target={'_blank'} rel={'noopener noreferrer'}>
-                whozinberg.org
-              </a>
-            </li>
-          </ul>
+              </a>{' '}
+              na{' '}
+              <a
+                href="https://gitlab.com/marc.fehr/community-isolation-map"
+                target="_blank"
+              >
+                GitLab
+              </a>{' '}
+              jako projekt Open Source, który podobnie jak ten, ma pomóc
+              lokalnej spłeczności w Muizenberg, w Południowej Afryce.
+              <h2>Jakie miejca można dodawać?</h2>
+              Wszystkie miejsca spełniajace lokalnego przedsiębiorstwa, czyli
+              restauracje, bary, kawiarnie, cukiernie, piekarnie, które oferuja
+              w czasie ograniczeń opcję na wynos lub dostawę (własna lub przez
+              platformy Glovo, Pyszne.pl, Uber Eats).
+              <h2>Co jeśli chcę usunąć dane z mapy</h2>
+              Jeśli nie chcesz, żeby miejsce nie było wyświetlane na mapie{' '}
+              <a href="mailto:krakowcommunityfoodmap@pm.me"><strong>napisz do mnie</strong></a>.
+              <h2>Czy strona będzie ulepszana?</h2>
+              Strona będzie cały czas rozwijana i ulepszana. Kazdy feedback jest
+              mile widziany, jeśli masz pomysł jak usprawnić działanie strony to
+              napisz.
+              <h2>Dlaczego tylko Kraków?</h2>
+              Kraków miejscem, w którym mieszkam i sam potrzebuję takiej mapy z
+              miejscami, gdzie mogę zamówić coś do jedzenia. Aplikacja jest
+              udostępniona na zasadach Open Srouce i kazdy moze postawić swoja
+              wersja dla swojego miasta – jeśli masz tylko wiedzę i odrobinę
+              ochoty.
+            </div>
+          </div>
         </div>
       </section>
     </Layout>
